471cdc5ffc2274a66b4ecdf31dbf945e45c0ca16
GriffinWood <gbw15@scarletmail.rutgers.edu>
2016-04-03 21:29:09 -0400

    Update sorted-list.c
    Changed sorted list to properly deal with the project. All that needs to be done is SLInsert needs to be called whenever a token is reached.

--- sorted-list.c
+++ sorted-list.c
@@ -2,23 +2,52 @@
 #include <stdio.h>
 #include "sorted-list.h"
 
-/* Allocate memory, set data fields to appropriate values*/
-SortedListPtr SLCreate(CompareFuncT cf, DestructFuncT df){
+SortedListPtr *list;
 
-	SortedListPtr SLPtr = (SortedListPtr) malloc(sizeof(struct SortedList));
+/* Allocate memory, set data fields to appropriate values*/
+void initializeSL(){
+
+	list = (SortedListPtr) malloc(sizeof(struct SortedList));
 
 	if(SLPtr == NULL) {
 		return NULL;
 	}
 
-	SLPtr->head = NULL;
-	SLPtr->compare = cf;
-	SLPtr->destroy = df;
-
-	
-	return SLPtr;
+	list->head = NULL;
+	list->compare = cf;
+	list->destroy = df;
 }
 
+typedef struct Record_ Record;
+struct Record_ {
+
+	char *fileName;
+	int occurrences;
+	Record *next;
+	Record *prev;
+};
+
+/* combined token with node
+Token creatToken(char * token){
+	Token *newToken = malloc(sizeof(Token));
+	newToken-> token= token;
+	return Token;
+}*/
+
+/*current file will be the most recent so you only need to check there since each file is only opened once*/
+void recordRecord(Node node, char *filename){
+	if(strcmp(node->head->filename, filename){
+		head->occurrences++;
+	}else{
+		Record *newRecord = malloc(sizeof(Record));
+		newRecord-> filename= filename;	
+		occurences = 1;
+		newRecord-> prev = NULL;
+		newRecord-> next = node-> head;
+		node-> head-> prev = NewRecord;
+		node-> head = newToken;
+	}
+}
 void printTree(Node *node){/*created for testing purposes only*/
         if(node->left != NULL)
                 printTree(node->left);
@@ -47,12 +76,18 @@ void SLDestroy(SortedListPtr list){
 	
 } 
 
-Node *createNode(void *newObj, Node *parent){
-
-
+Node *createNode(Node *parent, char *token, char *filename){
+	
+	Record *record = malloc(sizeof(Record));
 	Node *newNode = malloc(sizeof(Node));
-	newNode->data = newObj;
-    newNode->visited = 0;
+	
+	record->filename = filename;
+	record->occurance = 1;
+	record->next = NULL;
+	record->prev = NULL;
+	
+	newNode->token = token;
+    	newNode->head = record;
 	newNode->parent = parent;		
 	newNode->left = NULL;
 	newNode->right = NULL;
@@ -60,7 +95,7 @@ Node *createNode(void *newObj, Node *parent){
 	return newNode;
 }
 /*What is the purpose of parent here?*/
-int insertNode(CompareFuncT cf, Node *node, Node *parent, void *data){
+int insertToken(CompareFuncT cf, Node *node, Node *parent, char *token, char *filename){
 
 	Node *nodeParent = parent;
 	void *nodeData = node->data;
@@ -70,7 +105,7 @@ int insertNode(CompareFuncT cf, Node *node, Node *parent, void *data){
 
     /* Empty tree, build here and successful insertion*/
     if(node == NULL){ /*don't think this is needed anymore*/
-        node = createNode(data, node);
+        node = createNode(node, token, filename);
         return 1;
     }
 
@@ -78,7 +113,7 @@ int insertNode(CompareFuncT cf, Node *node, Node *parent, void *data){
     if(compareVal < 0){
     	/*nodeParent = node;*/
     	if(node -> left  == NULL){
-                node -> left = createNode(data, node);
+                node -> left = createNode(node, token, filename);
                 returnVal = 1;
         } else{
             returnVal = insertNode(cf, node->left, nodeParent, data);
@@ -90,7 +125,7 @@ int insertNode(CompareFuncT cf, Node *node, Node *parent, void *data){
     else if (compareVal > 0){   
     	/*nodeParent = node;   */
     	if(node -> right == NULL){
-                node -> right = createNode(data, node);
+                node -> right = createNode(node, token, filename);
                 returnVal = 1;
         } else{
            returnVal = insertNode(cf, node->right, nodeParent, data);
@@ -98,8 +133,9 @@ int insertNode(CompareFuncT cf, Node *node, Node *parent, void *data){
         }
     	
     } 
-    /* Duplicate found */
+    /* Word has already previously occurred  */
     else {
+    	recordRecord(Node,filename)
     	returnVal = 0;
     }
     
@@ -119,122 +155,6 @@ Node *minValueNode(Node* node)
     return current;
 }
 
-/* Must evaluate all three cases when there is no children,
-   one child or the most complex - two children
-   direction is negative if it came from the left side of its part and positive if it came from the right side*/
-int deleteNode(CompareFuncT cf, Node *root, void *data, int direction){
-    /* base case*/
-    if (root == NULL) return 0;
-    
-    int compareVal = cf(data, root->data);
-    /*int returnVal = 0;*/
- 
-    /* If the key to be deleted is smaller than the root's key,
-     then it lies in left subtree*/
-    if (compareVal < 0)
-        return deleteNode(cf, root->left, data, compareVal);
- 
-    /* If the key to be deleted is greater than the root's key,
-     then it lies in right subtree*/
-    else if (compareVal > 0)
-        return deleteNode(cf, root->right, data, compareVal);
- 
-    /*if key is same as root's key, then This is the node
-     to be deleted*/
-    else
-    {
-        /* No children */
-        if(root->left == NULL && root->right == NULL){
-
-            /* To account for deleting head where parent is NULL */
-            if(root->parent != NULL){
-               if(direction < 0){
-                    root->parent->left = NULL;
-                }
-                else if(direction > 0){
-                    root->parent->right = NULL;
-                }
-            }
-
-            free(root);
-            return 1;
-        }
-        /*node with only one child or no child*/
-        else if (root->left == NULL && root->right != NULL)
-        {
-
-            if(root->parent != NULL){
-                if(direction < 0){
-                    Node *temp = root->right;
-                    temp->parent = root->parent;
-                    root->parent->left = temp;
-                }
-                else if(direction > 0){
-                    Node *temp = root->right;
-                    temp->parent = root->parent;
-                    root->parent->right = temp;
-                }else{
-                    Node *temp = minValueNode(root->right);
-
-                    root->data = temp->data;
-
-                    return deleteNode(cf, root->right, temp->data, 0);
-                }
-
-            }
-
-
-            root = NULL;
-            free(root);
-            return 1;
-            
-
-        }
-        else if (root->right == NULL && root->left != NULL)
-        {
-            if(root->parent != NULL){
-                if(direction < 0){
-                    Node *temp = root->left;
-                    temp->parent = root->parent;
-                    root->parent->left = temp;
-                }
-                else if(direction > 0){
-                    Node *temp = root->left;
-                    temp->parent = root->parent;
-                    root->parent->left = temp;
-                }else{
-                    Node *temp = root->left; /*maxValueNode(root->left);*/
-                    while(temp->right != NULL){
-                            temp = temp->right;
-                    }
-
-                    root->data = temp->data;
-
-                    return deleteNode(cf, root->right, temp->data, 0);
-                }
-            }
-                     
-                
-            root = NULL;
-            free(root);
-            return 1;
-        }
- 
-        /* node with two children: Get the inorder successor (smallest
-         in the right subtree)*/
-        Node* temp = minValueNode(root->right);
- 
-        /* Copy the inorder successor's content to this node*/
-        
-        root->data = temp->data;
- 
-        /*Delete the inorder successor*/
-        return deleteNode(cf, root->right, temp->data, 0);
-    }
-    
-
-    return 0;
-}
 
 
 /* Uses the compare functions to determine where
@@ -247,120 +167,22 @@ int deleteNode(CompareFuncT cf, Node *root, void *data, int direction){
    are harder to do with linked lists. So either
    regular iteration or change implementation to use
    linked lists. */
-int SLInsert(SortedListPtr list, void *newObj){
+int SLInsert(char* token, char* filename){
 
 	int returnVal = 0;
 
 	/* Empty tree, make new one */
 	if(list->head == NULL){
-		list->head = createNode(newObj, NULL);
+		initializeSL();
+		list->head = createNode(NULL, token, filename);
 		return 1;
 	} 
 
-	returnVal = insertNode(list->compare, list->head, list->head, newObj);
+	returnVal = insertToken(list->compare, list->head, list->head, token, filename);
 	
 
 	return returnVal;
 }
 
 
-int SLRemove(SortedListPtr list, void *newObj){
-
-	/* Look for node and delete, return 1 on success or 0 otherwise */	
-	int returnVal = deleteNode(list->compare, list->head, newObj, 0);
-
-	return returnVal;
-}
-
-
-SortedListIteratorPtr SLCreateIterator(SortedListPtr list){
-
-
-	SortedListIteratorPtr listPtr = (SortedListIteratorPtr) malloc(sizeof(struct SortedListIterator));
-
-	listPtr->head = list->head;
-    listPtr->curr = list->head;
-    listPtr->type = 0;
-	listPtr->compare = list->compare;
-        listPtr->startData= NULL;
-
-	if(list->head == NULL)
-		listPtr->isFinished = 1;
-	else
-		listPtr->isFinished = 0;
-
-	return listPtr;
-
-}
-
-
-void SLDestroyIterator(SortedListIteratorPtr iter){
-
-	iter->curr = NULL;
-	free(iter->curr);
-	free(iter);
-	
-}
-
-/*
-int checkSubtree(Node *subtreeNode){
-
-    
-}*/
-/* Given the iterators current Node, traverse
-   right if possible, then left, or return current value */
-
-void * SLNextItem(SortedListIteratorPtr iter){
-    /*Node *head = iter->head;*/
-        Node *curr = iter->curr;
-    void *dataOutput;
-        if(iter->startData == NULL){
-                while(curr->right != NULL){
-                        curr = curr->right;
-                }
-                iter->curr = curr;
-                iter->startData = curr->data;
-                dataOutput = SLGetItem(iter);
-                return dataOutput;
-
-        }
-    if(curr->right != NULL){
-        if(iter->compare(iter->startData, curr->right->data) == 1){
-                curr = curr->right;
-                iter->curr = curr;
-                dataOutput = SLNextItem(iter);
-                return dataOutput;
-        }
-    }
-    /*Visit Current */
-    if(iter->compare(iter->startData, curr->data) == 1){
-        dataOutput = SLGetItem(iter);
-        return dataOutput;
-    }
-        if(curr->left != NULL){
-        if(iter->compare(iter->startData, curr->left->data) == 1){
-        curr = curr->left;
-        iter->curr = curr;
-        dataOutput = SLNextItem(iter);
-        return dataOutput;
-        }
-    }
-    
-    iter->curr = curr->parent;
-    dataOutput = SLNextItem(iter);
-   /*Reset current for future operations */
-    return dataOutput;
-}
-
-
-
-/* Used as helper function in SLNextItem */ 
-void * SLGetItem( SortedListIteratorPtr iter ){
-
-    /* Account for NULL values */
-    void *returnVal = iter->curr->data;
-        iter->startData = iter->curr->data;/*remembers the last data looked at*/
-        return returnVal;
-
-}
 
